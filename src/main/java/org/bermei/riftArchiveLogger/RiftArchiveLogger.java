24434331a32fc6a9d7894b7c2176a273624b1daf
sialim <syystonttu@gmail.com>
2026-08-29 20:35:58 -0700

    Initial commit

---
+++ src/main/java/org/bermei/riftArchiveLogger/RiftArchiveLogger.java
@@ -0,0 +1,16 @@
+package org.bermei.riftArchiveLogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RiftArchiveLogger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
